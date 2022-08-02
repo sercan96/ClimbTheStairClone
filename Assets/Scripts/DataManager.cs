11542b33c9a4bf2368f043522074765751e782f3
sercan1123 <38535473+sercan96@users.noreply.github.com>
2022-08-02 19:18:52 +0300

    commit

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
 
 
 public class DataManager : MonoBehaviour
 {
+    public Range Range;
     public CanvasUI CanvasUı;
     public static DataManager Instance;
     public int Money = 100;
@@ -20,18 +22,24 @@ public class DataManager : MonoBehaviour
     {
         EventManager.MousePressEvent -= IncreaseMoneyAmount;
     }
-    
+
+    void Awake()
+    {
+        GetMoney();
+        GetCylinder();
+    }
     void Start()
     {
         Instance = this;
         Debug.Log(Money);
-        GetMoney();
+
     }
 
     void Update()
     {
         CanvasUı.MyMoneyTxt.text = Money.ToString();
         SaveMoney();
+        SaveCylinder();
     }
     public void MainMoney(int amount)
     {
@@ -42,7 +50,6 @@ public class DataManager : MonoBehaviour
     public void IncreaseMoneyAmount()
     {
         Money++;
-
     }
 
     public void SaveMoney()
@@ -55,6 +62,42 @@ public class DataManager : MonoBehaviour
     {
         Money = PlayerPrefs.GetInt("Money");
     }
+
+    public void SaveCylinder()
+    {
+        Range.TargetCylinderState();
+        
+        PlayerPrefs.SetFloat("CylPositionY",Range.TargetCylinder.transform.position.y);  
+        PlayerPrefs.SetFloat("CylLocalScaleY",Range.TargetCylinder.transform.localScale.y);
+        
+        PlayerPrefs.SetFloat("CvsPositionY",Range.TargetCanvas.transform.position.y);
+        PlayerPrefs.SetFloat("CvsLocalScaleY",Range.TargetCanvas.transform.localScale.y);
+
+    }
+
+    public void GetCylinder()
+    {
+        float cylPositionY =PlayerPrefs.GetFloat("CylPositionY");
+        float cylLocalScaleY =PlayerPrefs.GetFloat("CylLocalScaleY");
+        
+        float cvsPositionY =PlayerPrefs.GetFloat("CvsPositionY");
+        float cvsLocalScaleY =PlayerPrefs.GetFloat("CvsLocalScaleY");
+
+        Vector3 targetPos = new Vector3(Range.TargetCanvas.transform.position.x, cylPositionY,
+            Range.TargetCanvas.transform.position.z);
+        
+        Vector3 targetScale = new Vector3(Range.TargetCanvas.transform.localScale.x, cylLocalScaleY,
+            Range.TargetCanvas.transform.localScale.z);
+        
+        Vector3 canvasPos = new Vector3(Range.TargetCanvas.transform.position.x, cvsPositionY,
+            Range.TargetCanvas.transform.position.z);
+        
+                
+        Vector3 canvasScale = new Vector3(Range.TargetCanvas.transform.localScale.x, cvsLocalScaleY,
+            Range.TargetCanvas.transform.localScale.z);
+        
+    }
+    
 
     
 }
--- Assets/Scripts/Range.cs
+++ Assets/Scripts/Range.cs
@@ -4,8 +4,12 @@ using UnityEngine.UI;
 
 public class Range : MonoBehaviour
 {
-    public GameObject MyRangeCanvas;
-    public Text CylinderScoreText;
+    public GameObject MainCanvas;
+    public GameObject TargetCanvas;
+    
+    public GameObject TargetCylinder;
+    public Text MainCylinderScoreText;
+    public Text TargetCylinderScoreText;
 
     private double _score;
     private float _riseValueY=0.2f;
@@ -24,10 +28,11 @@ public class Range : MonoBehaviour
 
     void Start()
     {
-        MyRangeCanvas = GameObject.Find("MyRange");
-        CylinderScoreText = GameObject.Find("MyScore").GetComponent<Text>();
+        // MainCanvas = GameObject.Find("MyRange");
+        // CylinderScoreText = GameObject.Find("MyScore").GetComponent<Text>();
         _score = 100;
     }
+
     
     public void IncreaseCylinderLength()
     {
@@ -37,14 +42,27 @@ public class Range : MonoBehaviour
         transform.localScale = new Vector3(transform.localScale.x,_riseValueY,transform.localScale.z);
         transform.position = new Vector3(transform.position.x,_riseValueY , transform.position.z);
         
-        MyRangeCanvas.transform.position = new Vector3(MyRangeCanvas.transform.position.x, 
-            MyRangeCanvas.transform.position.y + _riseMyRange, MyRangeCanvas.transform.position.z);
+        MainCanvas.transform.position = new Vector3(MainCanvas.transform.position.x, 
+            MainCanvas.transform.position.y + _riseMyRange, MainCanvas.transform.position.z);
     }
 
     public void IncreaseCylinderScore()
     {
         _score -= 0.2f;
         _score = Math.Round(_score,2);
-        CylinderScoreText.text ="-" + _score + "m";
+        MainCylinderScoreText.text ="-" + _score + "m"; 
+        TargetCylinderScoreText.text ="-" + _score + "m";
+    }
+
+
+    public void TargetCylinderState()
+    {
+        TargetCylinder.transform.position = transform.position;
+        TargetCylinder.transform.localScale = transform.localScale;
+
+        TargetCanvas.transform.position = MainCanvas.transform.position;
+        TargetCanvas.transform.localScale = MainCanvas.transform.localScale;
+
+        TargetCylinderScoreText = MainCylinderScoreText;
     }
 }
